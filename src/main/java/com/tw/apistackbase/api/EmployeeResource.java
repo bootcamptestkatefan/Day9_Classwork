d2bc3342c77ae0ff6423c45e76aa8ff917789536
bootcamptestkatefan <kate.fan@oocl.com>
2018-11-29 12:20:08 +0800

    Awwwwww i finished it lahhhh

--- src/main/java/com/tw/apistackbase/api/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/api/EmployeeResource.java
@@ -1,11 +1,10 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Employee;
-import com.tw.apistackbase.application.EmployeeService;
+import com.tw.apistackbase.core.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -13,16 +12,18 @@ import java.util.logging.Logger;
 public class EmployeeResource {
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
+ //   @Autowired
+ //   private EmployeeSvc employeeService;
     @Autowired
-    private EmployeeService employeeService;
+    private EmployeeRepository employeeRepository;
 
     @GetMapping(produces = {"application/json"})
-    public List<Employee> list() {
-        return employeeService.list();
+    public Iterable<Employee> list() {
+        return employeeRepository.findAll();
     }
 
     @PostMapping(produces = {"application/json"})
     public void add(@RequestBody Employee employee) {
-        employeeService.add(employee);
+        employeeRepository.save(employee);
     }
 }
---
+++ src/main/java/com/tw/apistackbase/core/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.tw.apistackbase.core;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
